13fd0f45d3c3ff277b55937b4b32451cf9742504
Himanshu Yengal <Himanshu.yengal@gmail.com>
2023-12-10 14:35:15 +0530

    added the landing page and made the basic structure of the app

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# job-tracker-app
 This is a project in which you can log in and store your jobs applications status combined with various graphs to analyze your progress.
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Landing } from "./pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="landing" element={<Landing />} />
+      </Routes>
+      <ToastContainer position="top-center" />
+    </BrowserRouter>
+  );
+}
+
+export default App;
