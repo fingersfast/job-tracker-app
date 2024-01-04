d9051631c5b6a8ac7c39d92ca3d3af5840074b46
Himanshu Yengal <Himanshu.yengal@gmail.com>
2024-01-04 14:54:10 +0530

    modified the app component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Landing, Register, Error } from "./pages";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import SharedLayout from "./pages/dashboard/SharedLayout";
-import { AddJob } from "./pages/dashboard";
-import AllJobs from "./pages/dashboard/AllJobs";
+import {
+  AddJob,
+  Profile,
+  AllJobs,
+  Stats,
+  SharedLayout,
+} from "./pages/dashboard";
 
 function App() {
   return (
@@ -20,8 +24,10 @@ function App() {
             </ProtectedRoute>
           }
         >
+          <Route index element={<Stats />} />
           <Route path="add-job" element={<AddJob />} />
           <Route path="all-jobs" element={<AllJobs />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
         <Route path="landing" element={<Landing />} />
         <Route path="register" element={<Register />} />
